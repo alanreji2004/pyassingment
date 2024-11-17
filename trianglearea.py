c93f473a3ad18210c0122e893fb0a874675a55e1
Alan Reji <143480799+alanreji2004@users.noreply.github.com>
2024-11-17 22:45:29 +0530

    Update trianglearea.py

--- trianglearea.py
+++ trianglearea.py
@@ -1,29 +1,31 @@
 import math
-choice = int(input("Enter choice\n1.Equilateral\n2.Isosceles\n3.Scalene\n"))
 
-if choice == 1:
-    side = int(input("Enter length of side: "))
-    area = (math.sqrt(3)/4) * (side ** 2)
-    print("The area of scalene Equilateral is: ",area)
-    
-elif choice == 2:
-    base = int(input("Enter the base of the triangle: "))
-    height = int(input("Enter the height of the triangle: "))
-    area = (1/2) * base * height
-    print("The area of scalene Isosceles is ",area)
-    
-elif choice == 3:
-    a = int(input("Enter the first side: "))
-    b = int(input("Enter the second side: "))
-    c = int(input("Enter the third side: "))    
-    
-    s =(a+b+c)/2 
-    
-    area = math.sqrt(s* (s-a)* (s-b)*(s-c))
-    if a + b > c and a + c > b and b + c > a:
-        print("The area of the scalene triangle is:", area)
+a = int(input("Enter the first side : "))
+b = int(input("Enter the second side : "))
+c = int(input("Enter the third side : "))
+
+if a == b == c:
+    area = (math.sqrt(3) / 4) * (a ** 2)
+    print("The area of the equilateral triangle is : ",area)
+
+elif a == b or b == c or a == c:
+    if a == b:
+        base = c
+        height = math.sqrt((a**2)-(base/2)**2)
+    elif b == c:
+        base = a
+        height = math.sqrt((b**2)-(base/2)**2)
     else:
-        print("The entered sides do not form a valid triangle.")    
+        base = b
+        height = math.sqrt((c**2)-(base/2)**2)
+        
+    area = (1/2) * base * height
+    print("The area of the isosceles triangle is : ",area)
 
 else:
-    print("invalid choice")
+    s = (a + b + c) / 2
+    if a + b > c and a + c > b and b + c > a:
+        area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+        print(f"The area of the scalene triangle is: {area:.2f}")
+    else:
+        print("The entered sides do not form a valid triangle.")
