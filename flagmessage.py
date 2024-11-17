222fd1a2944635b947f59c3754e6c89c246169d2
Alan Reji <143480799+alanreji2004@users.noreply.github.com>
2024-11-17 23:21:03 +0530

    Create flagmessage.py

---
+++ flagmessage.py
@@ -0,0 +1,10 @@
+flag = int(input("Enter the value of flag : "))
+
+if flag == 1:
+    print("Hot")
+elif flag == 2:
+    print("Luke warm")
+elif flag == 3:
+    print("Cold")
+else:
+    print("Out of range")
